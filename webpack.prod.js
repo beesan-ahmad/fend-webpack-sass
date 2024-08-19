a142816babde787e5e4598184af066aa20df0a53
beesan-ahmad <abumfarreh@gmail.com>
2024-08-19 18:45:21 +0300

    add a new plugins for webpack-prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,32 +1,48 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
     output: {
         libraryTarget: 'var',
-        library: 'Client'
+        library: 'Client',
     },
     mode: 'production',
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
-                loader: "babel-loader"
+                use: 'babel-loader',
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-            }
+                use: [
+                    MiniCssExtractPlugin.loader, // Extract CSS into separate files
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
         ],
-       
+    },
+    optimization: {
+        minimize: true, // Enable minimization for production
+        minimizer: [
+            new TerserPlugin(), // Minify JavaScript
+            new CssMinimizerPlugin(), // Minify CSS
+        ],
     },
     plugins: [
         new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+            template: './src/client/views/index.html',
+            filename: './index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css', // Extracted CSS file name
+        }),
+    ],
+};
